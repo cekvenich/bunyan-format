01436bca8d2a35c1386f1c1371f058801d8644c1
Vic <vic@eml.cc>
2020-05-26 09:30:03 -0400

    bump

--- terseB/terse-b.js
+++ terseB/terse-b.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.TerseB = void 0;
 const bunyan_1 = __importDefault(require("bunyan"));
 const bunyan_format2_1 = __importDefault(require("bunyan-format2"));
 class TerseB {
